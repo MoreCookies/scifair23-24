bca79e48a0553430828b4b1a5f3d55b61c9babf0
MoreCookies <derekrs222@gmail.com>
2023-12-08 14:58:52 -0800

    Debug model.py & update todo.txt

--- model.py
+++ model.py
@@ -29,29 +29,31 @@ foo = 64
 data = None #set data to this value
 class_num = data.shape()-2 #number of unique labels in data
 
-x = layers.Conv2D(foo, 3, padding="same")(data)
-x = layers.BatchNormalization()(x) #(x) --> using previous layer as input for next layer
-x = layers.Activation("relu")(x)
+
+
+model = layers.Conv2D(foo, 3, padding="same")(data)
+model = layers.BatchNormalization()(model) #(x) --> using previous layer as input for next layer
+model = layers.Activation("relu")(model)
 
 for filter in [64,128,256, 512, 1028]:
-    model = layers.Conv2D(filter, 3, padding="same",activation="relu")(x) # (filters, kernel_size, padding, activation)
-    model = layers.BatchNormalization()(x)
+    model = layers.Conv2D(filter, 3, padding="same",activation="relu")(model) # (filters, kernel_size, padding, activation)
+    model = layers.BatchNormalization()(model)
 
-    model = layers.Conv2D(filter, 3, padding="same",activation="relu")(x)
-    model = layers.BatchNormalization()(x)
+    model = layers.Conv2D(filter, 3, padding="same",activation="relu")(model)
+    model = layers.BatchNormalization()(model)
 
-    model = layers.MaxPooling2D(3, strides=2, padding="same")(x) # (pool_size, strides, padding)
+    model = layers.MaxPooling2D(3, strides=2, padding="same")(model) # (pool_size, strides, padding)
 
 #expanding u-net
 
 for filter in [1028, 512, 256, 128, 64]:
-    model = layers.Conv2DTranspose(filter, 3, padding="same",activation="relu")(x)
-    model = layers.BatchNormalization()(x)
+    model = layers.Conv2DTranspose(filter, 3, padding="same",activation="relu")(model)
+    model = layers.BatchNormalization()(model)
 
-    model = layers.Conv2DTranspose(filter, 3, padding="same",activation="relu")(x)
-    model = layers.BatchNormalization()(x)
+    model = layers.Conv2DTranspose(filter, 3, padding="same",activation="relu")(model)
+    model = layers.BatchNormalization()(model)
 
-    model =  layers.UpSampling2D(2)(x)
+    model =  layers.UpSampling2D(2)(model)
 
 classification = layers.Conv2D(class_num, 3, activation="softmax", padding="same")(x)
 
